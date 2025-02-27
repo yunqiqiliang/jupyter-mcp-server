87f6a492456fba999d41ab2c63cd6d0444471164
Eléonore Charles <eleonore@echarles.net>
2025-02-27 16:49:44 +0100

    earth data download tools

--- jupyter_mcp_server/server.py
+++ jupyter_mcp_server/server.py
@@ -1,9 +1,9 @@
+import os
 import logging
 
 from jupyter_nbmodel_client import NbModelClient, get_jupyter_notebook_websocket_url
 from jupyter_kernel_client import KernelClient
 from mcp.server.fastmcp import FastMCP
-import os
 
 # Initialize FastMCP server
 mcp = FastMCP("notebook")
@@ -18,6 +18,27 @@ logger = logging.getLogger(__name__)
 kernel = KernelClient(server_url=SERVER_URL, token=TOKEN)
 kernel.start()
 
+def extract_output(output: dict) -> str:
+    """Extract output from a Jupyter notebook cell.
+    
+    Args:
+        output: Output dictionary
+    
+    Returns:
+        str: Output text
+    """
+    if output["output_type"] == "display_data":
+        return output["data"]["text/plain"]
+    elif output["output_type"] == "execute_result":
+        return output["data"]["text/plain"]
+    elif output["output_type"] == "stream":
+        return output["text"]
+    elif output["output_type"] == "error":
+        return output["traceback"]
+    else:
+        return ""
+
+
 @mcp.tool()
 def add_execute_code_cell(cell_content: str) -> str:
     """Add and execute a code cell in a Jupyter notebook.
@@ -26,7 +47,7 @@ def add_execute_code_cell(cell_content: str) -> str:
         cell_content: Code content
         
     Returns:
-        str: Success message
+        str: Cell output
     """
     notebook = NbModelClient(get_jupyter_notebook_websocket_url(server_url=SERVER_URL, token=TOKEN, path=NOTEBOOK_PATH))
     notebook.start()
@@ -34,9 +55,16 @@ def add_execute_code_cell(cell_content: str) -> str:
     cell_index = notebook.add_code_cell(cell_content)
     notebook.execute_cell(cell_index, kernel)
     
+    ydoc = notebook._doc
+    outputs = ydoc._ycells[cell_index]["outputs"]
+    if len(outputs) == 0:
+        cell_output = ""
+    else:
+        cell_output = [extract_output(output) for output in outputs]
+            
     notebook.stop()
     
-    return "Cell executed"
+    return cell_output
         
         
 @mcp.tool()
@@ -58,6 +86,51 @@ def add_markdown_cell(cell_content: str) -> str:
     return "Markdown cell added"
 
 
+@mcp.tool()
+def download_earth_data_granules(folder_name: str, short_name: str, count: int, temporal: tuple, bounding_box: tuple) -> str:
+    """Add a code cell in a Jupyter notebook to download Earth data granules from NASA Earth Data.
+    
+    Args:
+        folder_name: Local folder name to save the data.
+        short_name: Short name of the Earth dataset to download.
+        count: Number of data granules to download.
+        temporal: (Optional) Temporal range in the format (date_from, date_to).
+        (Optional) Bounding box in the format (lower_left_lon, lower_left_lat, upper_right_lon, upper_right_lat).
+        
+    Returns:
+        str: Cell output
+    """
+        
+    search_params = {
+        "short_name": short_name,
+        "count": count,
+        "cloud_hosted": True
+    }
+
+    if temporal and len(temporal) == 2:
+        search_params["temporal"] = temporal
+    if bounding_box and len(bounding_box) == 4:
+        search_params["bounding_box"] = bounding_box
+        
+    cell_content = f"""import earthaccess
+earthaccess.login()
+
+search_params = {search_params}  # Pass dictionary as a variable
+results = earthaccess.search_data(**search_params)
+files = earthaccess.download(results, "./{folder_name}")"""
+        
+    notebook = NbModelClient(get_jupyter_notebook_websocket_url(server_url=SERVER_URL, token=TOKEN, path=NOTEBOOK_PATH))
+    notebook.start()
+    
+    cell_index = notebook.add_code_cell(cell_content)
+    
+    notebook.execute_cell(cell_index, kernel)
+    
+    notebook.stop()
+    
+    return f"Data downloaded in folder {folder_name}"
+
 if __name__ == "__main__":
     # Initialize and run the server
     mcp.run(transport='stdio')
+    
